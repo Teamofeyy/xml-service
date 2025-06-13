ccb4bada9e1775c36d1d27694d033810828c6640
Teamofeyy <timarnd06@gmail.com>
2025-06-13 22:17:11 +0300

    Timeout changed

--- run.sh
+++ run.sh
@@ -5,8 +5,8 @@ docker pull postgres:12
 
 docker-compose up -d
 
-echo "Ожидаем запуска сервисов (20 секунд)..."
-sleep 20
+echo "Ожидаем запуска сервисов (5 секунд)..."
+sleep 5
 
 echo "Проверяем работу сервисов:"
 docker ps --format "table {{.Names}}\t{{.Status}}\t{{.Ports}}"
